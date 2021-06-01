17ff34c889f163709f7bde4688c69c59070c1515
David <tensdavid@gmail.com>
2021-06-01 16:21:52 +0400

    Initial Commit

---
+++ Covid19 app/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Covid19 app
+//
+//  Created by David T on 6/1/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
